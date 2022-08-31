a28a12aa06d719528a439b3e18adb6640d32eea7
llvvllvv <109870903+llvvllvv@users.noreply.github.com>
2022-08-31 10:57:18 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://e2447354.eu-gb.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://8614cce7.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
